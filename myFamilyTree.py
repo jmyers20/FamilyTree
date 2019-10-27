d78a01a65573e418a8b4d92515b6f020f6c843a7
Dymon Moore <dymon.mooredcs@gmail.com>
2019-10-27 10:01:54 -0400

    initial ancestors code

--- myFamilyTree.py
+++ myFamilyTree.py
@@ -1,4 +1,4 @@
-#Programming Assignment #2
+#Prog;l=ramming Assignment #2
 #$> python3 myFamilyTree.py test1.txt output1.txt
 
 import sys
@@ -17,21 +17,23 @@ def addPeople( names ):
     #add logic for E name1 name2 or E name1 name2 name3 here
 
     #check to see if name already exists, if not, add it to the dictionary
-    for x in range (1,len(names)):
-        if not names[x] in people.keys():
+   print( names) 
+   for x in range (1,len(names)):
+         if not names[x] in people.keys():
             people[names[x]] = {}
             people[names[x]]['name'] = names[x]
             people[names[x]]['parents'] = []
             people[names[x]]['spouse'] = []
             people[names[x]]['children'] = []
+            people[names[x]]['ancestors'] = []
 
-    if len(names) == 3:
+   if len(names) == 3:
         #add each person to each other's spouses
         if not names[2] in people[names[1]]['spouse']:
             people[names[1]]['spouse'].append(names[2])
             people[names[2]]['spouse'].append(names[1])
 
-    if len(names) == 4:
+   if len(names) == 4:
         #add each person to each other's spouses
         if not names[2] in people[names[1]]['spouse']:
             people[names[1]]['spouse'].append(names[2])
@@ -48,7 +50,6 @@ def addPeople( names ):
 def isSibling( names ):
     # X name1 sibling name2
     output.write('X ' + names[1] + ' sibling ' + names[3] + '\n')
-
     result = 'No \n'
 
     if not names[1] in people.keys() or not names[3] in people.keys():
@@ -57,39 +58,34 @@ def isSibling( names ):
     else:
         personOneParents = people[names[1]]['parents']
         personTwoParents = people[names[3]]['parents']
-
-        for x in personOneParents:
+     
+        for x in personOneParents: 
             for y in personTwoParents:
-                if x == y:
-                    result = 'Yes \n'
+            	 if x == y:
+	             	 result = 'Yes \n'
 
     output.write(result + '\n')
 
-def returnSiblings( names ):
-    # W sibling name1
+def isAncestorHelper(names):
+     output.write('X ' + names[1] + ' sibling ' + names[3] + '\n')
+     ancestor = people[names[1]]
+     descendant = people[names[3]]	
+     isAncestor(ancestor, descendant)
 
-    output.write('W sibling ' + names[2] + '\n')
-    list = []
 
-    result = ''
-    if not names[2] in people.keys():
-        result += '\n'
+def isAncestor( ancestor , person ):
+    #X name1 ancestor name 3
+    result = 'No \n'
+    if person['parents'] == []:
+ 	return false
+    else:
+	if person['parents'][0] == ancestor :
+		print('here')
+	else:
+		ancestorLevelUp = person['parents'][0]
+		resultSideOne = isAncestor(ancestorLevelUp, person)
 
-    personOneParents = people[names[2]]['parents']
 
-    siblings = []
-    for i in range(0,len(personOneParents)):
-        siblings += people[personOneParents[i]]['children']
-
-    #remove duplicates and names[2] from siblings
-    siblings = set(siblings)
-    siblings.remove(names[2])
-    sorted(siblings)
-
-    for value in siblings:
-        result += value + '\n'
-
-    output.write(result + '\n')
 
 if __name__ == '__main__':
     if(len(sys.argv) != 3):
@@ -118,9 +114,10 @@ if __name__ == '__main__':
                 elif(line[0] == 'X'):
                     if line[2] == 'sibling':
                         isSibling(line)
+                    if line[2] == 'ancestor':
+                        isAncestorHelper(line)
                 elif(line[0] == 'W'):
-                    if line[1] == 'sibling':
-                        returnSiblings(line)
+                    print(line)                     #replace with method calls
 
         #myprint( people )
         output.close()
